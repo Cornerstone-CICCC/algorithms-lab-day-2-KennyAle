785b53e2b4e9b6123a03de508ee91208a3b71672
KennyAle <kennyviver@gmail.com>
2024-10-01 11:30:30 -0700

    Exercise 9

--- ex09.js
+++ ex09.js
@@ -2,5 +2,19 @@
 // and a property name, and returns the array sorted by that property in ascending order.
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
+const sortByProperty = function (people, order) {
+    switch (order) {
+        case "age":
+            return people.sort((a, b) => {
+                return a.age - b.age
+            })
+        case "name":
+            return people.sort((a, b) => 
+                (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)
+            )
+        default:
+            break;
+    }
+}
 
 console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
