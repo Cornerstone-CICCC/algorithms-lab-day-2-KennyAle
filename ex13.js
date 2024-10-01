4813ee9243f8d59f534be2fdfd5a015ee305bc88
KennyAle <kennyviver@gmail.com>
2024-10-01 13:36:19 -0700

    Exercise 13 Refactor

--- ex13.js
+++ ex13.js
@@ -3,12 +3,17 @@
 // Example: sumRange(5) should return 15 (1 + 2 + 3 + 4 + 5).
 
 const sumRange = function (n) {
-    let numbers = []
-    while (n != 0) {
-        numbers.push(n)
-        n--
+    // let numbers = []
+    // while (n != 0) {
+    //     numbers.push(n)
+    //     n--
+    // }
+    // return numbers.reduce((acc, curr) => acc + curr, 0)
+    if (n === 0) {
+        return 0
     }
-    return numbers.reduce((acc, curr) => acc + curr, 0)
+
+    return n + sumRange(n - 1)
 }
 
 console.log(sumRange(5)); // Expected output: 15
