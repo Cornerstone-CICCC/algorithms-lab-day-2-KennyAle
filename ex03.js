aae33ca962c81c2189284387571d3606905eecac
KennyAle <kennyviver@gmail.com>
2024-10-01 09:38:21 -0700

    Exercise 3

--- ex03.js
+++ ex03.js
@@ -2,4 +2,13 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+const findMissingNumber = function(numbers) {
+    console.log(numbers)
+    for (let i = 0; i < numbers.length -1; i++) {
+        if (numbers[i] + 1 != numbers [i + 1]) {
+            return numbers[i] + 1
+        }
+    }
+}
+
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
