8a6d303a71b0458e9bec00fb000bff519ab5a877
KennyAle <kennyviver@gmail.com>
2024-10-01 09:17:09 -0700

    Exercise 2

--- ex02.js
+++ ex02.js
@@ -2,4 +2,15 @@
 // containing the elements that are in the first array but not in the second array.
 // Example: arrayDifference([1, 2, 3], [2, 3, 4]) should return [1].
 
+const arrayDifference = function(array, array2) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (!array2.includes(element)) {
+            result.push(element)
+        }
+    }
+    return result
+}
+
 console.log(arrayDifference([1, 2, 3], [2, 3, 4])); // Expected output: [1]
