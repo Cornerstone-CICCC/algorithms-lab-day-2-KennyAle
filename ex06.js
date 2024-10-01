d2297b96d1c4ef0037f55e99a380a75efda06166
KennyAle <kennyviver@gmail.com>
2024-10-01 12:59:33 -0700

    Exercise 6

--- ex06.js
+++ ex06.js
@@ -3,4 +3,17 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+const getQueryParams = function (query) {
+    let result = {}
+    let url = query.split("?")
+    let noUrl = url[1]
+    let pairs = noUrl.split("&")
+    pairs.forEach(keys => {
+        let keyValues = keys.split("=")
+        console.log(keyValues)
+        result[keyValues[0]] = keyValues[1]   
+        console.log(result)
+    });
+}
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
