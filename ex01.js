a42d7a64c2bfadc534760f52407cacf3c1498476
KennyAle <kennyviver@gmail.com>
2024-10-01 09:01:59 -0700

    Exercise 1

--- ex01.js
+++ ex01.js
@@ -2,4 +2,16 @@
 // with the first letter of each word capitalized.
 // Example: capitalizeWords("hello world") should return "Hello World".
 
+const capitalizeWords = function(text) {
+    words = text.split(" ")
+    result = ""
+    for (let i = 0; i < words.length; i++) {
+        let element = words[i];
+        let [firstLetter, ...letters] = element
+        let capitalize = firstLetter.toUpperCase() + letters.join("")
+        result += ` ${capitalize}`
+    }
+    return result
+}
+
 console.log(capitalizeWords("hello world")); // Expected output: "Hello World"
